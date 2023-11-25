12c49ab2de74d4c66a2912ad1697ea28dd8d7152
Alessandro Folloni <allefollo@gmail.com>
2023-11-24 22:00:41 -0500

    Add files via upload

---
+++ vander.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n=35
+x_vander=np.arange(1,n)
+A_full=np.vander(x_vander, n)
+print(A_full)
+rel_err=np.zeros((n-2,))
+k_2=np.zeros((n-2,))
+k_inf=np.zeros((n-2,))
+
+for i in range(2,n):
+    x_true=np.ones((i,))
+    A=A_full[:i,:i]
+    b=x_true@A
+
+    k_2[i-2]=np.linalg.cond(A,2)
+    k_inf[i-2]=np.linalg.cond(A,np.inf)
+    x=np.linalg.solve(A,b)
+
+    rel_err[i-2]=np.linalg.norm(x_true-x,2)/np.linalg.norm(x_true,2)
+
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.title("Relative error")
+plt.xlabel("n")
+plt.ylabel("Relative Error")
+plt.plot(np.arange(2,n), rel_err)
+plt.grid()
+
+plt.subplot(1, 2, 2)
+plt.title("condition numbers")
+plt.xlabel("n")
+plt.ylabel("k")
+plt.plot(np.arange(2,n), k_2)
+plt.plot(np.arange(2,n), k_inf)
+plt.legend(['k_2', 'k_inf'])
+plt.grid()
+
+plt.show()
+
